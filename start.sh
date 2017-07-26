900daba9a870bb68dffbc61d379e62cd38d180cd
Dr. Waldijk <DokterW@users.noreply.github.com>
2017-07-26 22:02:58 +0200

    cr

--- start.sh
+++ start.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-# geektime v0.1
+# geektime v0.2
 # Made by Dr. Waldijk
 # See Unix Time, Internet Time and local time.
 # Read the README.md for more info.
 # By running this script you agree to the license terms.
 # -----------------------------------------------------------------------------------
+GETINAM="geektime"
+GETIVER="0.2"
 while :; do
     GETIUT=$(date +%s)
     GETILH=$(date -d @$GETIUT +%H)
@@ -15,11 +17,16 @@ while :; do
     GETILSU=$(date -u -d @$GETIUT -d "1 hour" +%S)
     GETIIT=$(echo "($GETILS+($GETILM*60)+($GETILH*3600))/86.4" | bc)
     clear
-    echo " Unix Time: $GETIUT"
-    echo "     .beat: $GETIIT"
-    echo "Local time: $GETILH:$GETILM:$GETILS"
+    echo " Unix: $GETIUT"
+    echo ".beat: $GETIIT"
+    echo -n "Local: $GETILH:$GETILM:$GETILS "
     read -t 15 -s -n1 GETIKEY
     case "$GETIKEY" in
+        [iI])
+            echo ""
+            clear
+            break
+        ;;
         [qQ])
             echo ""
             clear
